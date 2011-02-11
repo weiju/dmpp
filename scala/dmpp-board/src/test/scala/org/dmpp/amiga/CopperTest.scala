a4774a325bff0aa20b164ad0223e3220435676b0
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-02-11 06:45:10 -0800

    Copper is executing its first (simple) copper list correctly

--- scala/dmpp-board/src/test/scala/org/dmpp/amiga/CopperTest.scala
+++ scala/dmpp-board/src/test/scala/org/dmpp/amiga/CopperTest.scala
@@ -4,25 +4,55 @@ import org.specs._
 import org.specs.runner.{ConsoleRunner, JUnit4}
 import org.mahatma68k.AddressSpace
 
+case class CopperList(address: Int, words: List[Int]) {
+  def contains(anAddress: Int) = {
+    anAddress >= address && anAddress < address + words.length * 2
+  }
+  def readShort(anAddress: Int) = words((anAddress - address) / 2)
+}
+
 /**
- * A test for Copper functionality.
+ * A custom mock memory class which simulates mockup copper lists
+ * and logs writes that the copper performs.
  */
-class MockMemory extends AddressSpace {
+class CopperListMemory extends AddressSpace {
+
+  private var copperLists: List[CopperList] = Nil
+  var writeLog : List[String] = Nil
+
   def start = 0
   def size  = 10000000
   def readByte(address: Int)  = 0
-  def readShort(address: Int) = 0
+  def readShort(address: Int): Int = {
+    for (copperList <- copperLists) {
+      if (copperList.contains(address)) return copperList.readShort(address)
+    }
+    0
+  }
   def readLong(address: Int)  = 0
   def writeByte(address: Int, value: Int) { }
-  def writeShort(address: Int, value: Int) { }
-  def writeLong(address: Int, value: Int) { }        
+  def writeShort(address: Int, value: Int) {
+    addLog("#%d.w -> $%04x".format(value, address))
+  }
+  private def addLog(log: String) = {
+    writeLog ::= log
+    writeLog reverse
+  }
+  def writeLong(address: Int, value: Int) { }
+
+  def addCopperList(copperList: CopperList) {
+    copperLists ::= copperList
+  }
 }
 
+/**
+ * A test for Copper functionality.
+ */
 class CopperTest extends JUnit4(BlitterLogicSpec)
 object CopperSpec extends Specification {
   val NoCyclesUsed = 0
 
-  val mockMemory = new MockMemory
+  val mockMemory = new CopperListMemory
   var copper: Copper = null
 
   "Copper" should {
@@ -58,5 +88,23 @@ object CopperSpec extends Specification {
       copper.waiting must beFalse
       copper.enabled must beFalse
     }
+    "will execute a move instruction" in {
+      // first copper instruction in the HRM: a move of #$02 into
+      // $dff0e0 (BPL1PTH)
+      val copperList = CopperList(0x20000, List(0x00e0, 0x0002))
+      addCopperListAndRestart(copperList)
+
+      copper.doDma must_== Copper.NumMoveCycles
+      mockMemory.writeLog.length must_== 1
+      mockMemory.writeLog(0) must_== "#2.w -> $dff0e0"
+    }
+  }
+
+  private def addCopperListAndRestart(copperList: CopperList) {
+    mockMemory.addCopperList(copperList)
+    copper.cop1lcl.value = 0x0000
+    copper.cop1lch.value = 0x0002
+    copper.restartOnVerticalBlank
+    copper.enabled = true
   }
 }
