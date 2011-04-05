df41dbd478b356706e9db32c1f7054dcd7f7b890
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-04-04 21:52:52 -0700

    outfactored base abstractions to common package

--- scala/project/build/Dmpp.scala
+++ scala/project/build/Dmpp.scala
@@ -6,8 +6,12 @@ class DmppProject(info: ProjectInfo) extends ParentProject(info) {
   val mahatma68k = "org.mahatma68k" % "mahatma68k" % "1.0-SNAPSHOT" % "compile->default"
   val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
 
-  lazy val cpu   = project("dmpp-cpu", "dmpp-cpu")
-  lazy val cymus = project("dmpp-cymus", "dmpp-cymus", cpu)
-  lazy val board = project("dmpp-board", "dmpp-board", cymus, cpu)
-  lazy val debugger = project("dmpp-debugger", "dmpp-debugger", cymus, board)
+  lazy val common   = project("dmpp-common", "dmpp-common")
+  lazy val cpu      = project("dmpp-cpu", "dmpp-cpu", common)
+  lazy val cymus    = project("dmpp-cymus", "dmpp-cymus", common)
+  lazy val board    = project("dmpp-board", "dmpp-board",
+                              cymus, cpu, common)
+  lazy val debugger = project("dmpp-debugger", "dmpp-debugger",
+                              cymus, board, common)
 }
+
